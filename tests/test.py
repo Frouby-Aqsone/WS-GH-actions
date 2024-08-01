3a34d50699fb2e2134ee28bcfc9b3b3fa1f93848
Frouby-Aqsone <135623898+Frouby-Aqsone@users.noreply.github.com>
2024-08-01 10:39:09 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,11 @@
 """This module runs some unit test"""
 
 def inc(x):
-    """Increments and returns the value x"""
+    """Increments and returns the value x
+
+    Keyword arguments:
+    x -- the number we want to increment
+    """
     return x + 1
 
 
